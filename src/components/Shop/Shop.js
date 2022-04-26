e5c665843ff143001916771ece2cba643b4f5385
aonikpaulalock <alockpaul9845@gmail.com>
2022-04-26 21:31:35 +0600

    first commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,15 +7,35 @@ import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const [products, setProducts] = useProducts();
     const [cart, setCart] = useState([]);
+    const [pages, setPages] = useState(0)
+    const [page, setPage] = useState(0)
+    const [size, setSize] = useState(10)
+    const [products, setProducts] = useState([]);
 
-    useEffect( () =>{
+    useEffect(() => {
+        const url =`http://localhost:5000/products?page=${page}&size=${size}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, [page,size]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/productCount")
+            .then(res => res.json())
+            .then(data => {
+                const count = data.productCount;
+                const page = Math.ceil(count / 10);
+                setPages(page)
+            })
+    }, [])
+
+    useEffect(() => {
         const storedCart = getStoredCart();
         const savedCart = [];
-        for(const id in storedCart){
-            const addedProduct = products.find(product => product.id === id);
-            if(addedProduct){
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product._id === id);
+            if (addedProduct) {
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
                 savedCart.push(addedProduct);
@@ -24,33 +44,33 @@ const Shop = () => {
         setCart(savedCart);
     }, [products])
 
-    const handleAddToCart = (selectedProduct) =>{
+    const handleAddToCart = (selectedProduct) => {
         console.log(selectedProduct);
         let newCart = [];
-        const exists = cart.find(product => product.id === selectedProduct.id);
-        if(!exists){
+        const exists = cart.find(product => product._id === selectedProduct._id);
+        if (!exists) {
             selectedProduct.quantity = 1;
             newCart = [...cart, selectedProduct];
         }
-        else{
-            const rest = cart.filter(product => product.id !== selectedProduct.id);
+        else {
+            const rest = cart.filter(product => product._id !== selectedProduct._id);
             exists.quantity = exists.quantity + 1;
             newCart = [...rest, exists];
         }
-        
+          console.log(setCart(newCart));
         setCart(newCart);
-        addToDb(selectedProduct.id);
+        addToDb(selectedProduct._id);
     }
 
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product=><Product 
-                        key={product.id}
+                    products.map(product => <Product
+                        key={product._id}
                         product={product}
                         handleAddToCart={handleAddToCart}
-                        ></Product>)
+                    ></Product>)
                 }
             </div>
             <div className="cart-container">
@@ -59,6 +79,24 @@ const Shop = () => {
                         <button>Review Order </button>
                     </Link>
                 </Cart>
+            </div>
+            <div>
+                <div className='pagination'>
+                    {
+                        [...Array(pages).keys()]
+                            .map(number =>
+                                <button className={page === number ? "selected" : ""} onClick={() => setPage(number)}>
+                                    {number}
+                                </button>)
+                    }
+                    <select onChange={(e) => setSize(e.target.value)}>
+                        <option value="5">5</option>
+                        <option value="10" selected>10</option>
+                        <option value="30">30</option>
+                        <option value="50">50</option>
+                    </select>
+
+                </div>
             </div>
         </div>
     );
